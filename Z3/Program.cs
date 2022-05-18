10fd1da4e627b4d057980b4a0e91a881dc1401c1
marmeladka63 <peshkova-va@yandex.ru>
2022-05-18 19:15:49 +0300

    Создали файл вывода массива совместо с салюшин

--- Z3/Program.cs
+++ Z3/Program.cs
@@ -1,3 +1,19 @@
 ﻿using MyClasses;
+using System;
+Console.Clear();
+int n=int.Parse(Console.ReadLine());
+int m=int.Parse(Console.ReadLine());
 int[,] array=MyExample.GetArray(n,m,0,100);
+
+for (int i=0; i<n; i++)
+{
+    
+for (int j=0; j<m; j++)
+{
+        Console.Write($"{array[i,j]} ");
+
+}
+    Console.WriteLine();
+}
+
 
