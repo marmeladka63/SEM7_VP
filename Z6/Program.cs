f6179c1d2a23b44ee2b5f25f1558c9a7d20ae09a
marmeladka63 <peshkova-va@yandex.ru>
2022-05-18 21:36:39 +0300

    Создать двумерный массив n строк, m столбцов.Найти сумму главной диагонали

---
+++ Z6/Program.cs
@@ -0,0 +1,54 @@
+﻿// Создать двумерный массив n строк, m столбцов
+// Найти сумму главной диагонали
+using System;
+using static System.Console;
+Console.Clear();
+
+
+int[,] startArray = GetRandomArray(4, 5, 1, 10);
+PrintArray(startArray);
+WriteLine(GetSumDiag(startArray));
+
+int[,] GetRandomArray(int rows, int columns, int min, int max)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            result[i, j] = new Random().Next(min, max);
+    }
+    return result;
+}
+
+
+
+
+
+
+int GetSumDiag(int[,] array)
+{
+    int sum = 0;
+    int length = array.GetLength(0) < array.GetLength(1) ? array.GetLength(0) : array.GetLength(1);
+
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i, i];
+    }
+    return sum;
+}
+
+
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
