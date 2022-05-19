1e4d7503a70900dfdba515ebae3570d81438075d
marmelaka63 <peshkova-va@yandex.ru>
2022-05-19 16:41:39 +0300

    эксперемент

---
+++ Z7/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+using static System.Console;
+//Console.Clear();
+
+static void Main(string[] args)
+        {
+            int[,] arr = {{ 1, 2, 3, 4 }, { 5, 6, 7, 8}}; //Массив
+            int find = 25; //Искомое число
+            int m=0, n=0;
+            bool f = false;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (find == arr[i, j]) { m = i;n = j;f = true; };
+                    Console.Write("{0}\t", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            if (f)
+                Console.WriteLine($"Число найдено. Строка {m+1} Столбец {n+1}");
+            else
+                Console.WriteLine("Число не найдено");
+ 
+            Console.ReadKey();
+        }
