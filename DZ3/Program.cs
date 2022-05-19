b2358b43d2fecb5e6182de73e04b148c20d11c00
marmelaka63 <peshkova-va@yandex.ru>
2022-05-19 16:43:39 +0300

    //Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет с заданными значениями

---
+++ DZ3/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+/*Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет*/
+
+
+
+
+using System;
+using static System.Console;
+Console.Clear();
+
+int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+Console.Write("Ввведите номер строки i=");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите номер столбца j=");
+int m = int.Parse(Console.ReadLine());
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        if (i == n && j == m)
+        {
+            Console.WriteLine(arr[n, m]);
+        }
+        else
+        {
+
+            Console.WriteLine("Такого числа нет");
+            }
+
+    }
+    
+}
+
+
+
+
