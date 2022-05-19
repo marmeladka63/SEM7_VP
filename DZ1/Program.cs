856fc1c82c6226587734e95c21a02a0dc3213010
marmelaka63 <peshkova-va@yandex.ru>
2022-05-19 16:42:26 +0300

    //  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.

---
+++ DZ1/Program.cs
@@ -0,0 +1,38 @@
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+using System;
+
+
+Console.Clear();
+int n=int.Parse(Console.ReadLine());
+int m=int.Parse(Console.ReadLine());
+
+double[,] array=GetArray(n,m,-100,100);
+
+
+for (int i=0; i<n; i++)
+{
+    
+for (int j=0; j<m; j++)
+{
+        Console.Write($"{array[i,j]} ");
+
+}
+    Console.WriteLine();
+}
+
+
+
+
+double[,] GetArray(int rows,int columns, int min, int max)
+    {
+        double[,] result=new double[rows,columns];
+
+        for(int i=0; i<rows; i++)
+        {
+            for(int j=0;j<columns;j++)
+            {
+                result[i,j]=new Random().Next(min,max);
+            }
+        }
+        return result;
+    }
